85dcb4e9e02b6a948b11c748e4cfd6e22e23c09a
komarathe <komalmarathe2911@gmail.com>
2021-07-26 13:40:24 -0700

    Add problem link and statement per review

--- leetcode/1221_split_string_in_balanced_strings.py
+++ leetcode/1221_split_string_in_balanced_strings.py
@@ -1,3 +1,10 @@
+# Refer: https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+# Balanced strings are those that have an equal quantity of 'L' and 'R'
+# characters. Given a balanced string s, split it in the maximum amount of
+# balanced strings.
+# Return the maximum amount of split balanced strings.
+
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
         main_count = 0
